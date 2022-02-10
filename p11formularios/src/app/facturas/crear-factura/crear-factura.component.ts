fcf5d02c024ce33a22e5576131be0cb369a4b10a
samuelsanz <samuel.sanz@gruposantander.com>
2022-02-10 11:31:16 +0000

    final del ejercicio

--- p11formularios/src/app/facturas/crear-factura/crear-factura.component.ts
+++ p11formularios/src/app/facturas/crear-factura/crear-factura.component.ts
@@ -1,6 +1,8 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ValidateCif } from 'src/app/validators/cif.validator';
+import Big from 'big.js';
 
 @Component({
   selector: 'app-crear-factura',
@@ -16,8 +18,30 @@ export class CrearFacturaComponent implements OnInit {
   ngOnInit(): void {
     this.formFactura = new FormGroup({
       nombreCliente: new FormControl('', [Validators.required, Validators.minLength(4)]), 
-      cifCliente: new FormControl('', [Validators.required, ValidateCif])
+      cifCliente: new FormControl('', [Validators.required, ValidateCif]),
+      fechaFactura: new FormControl((new Date()).toISOString().substring(0,10)),
+      baseImponible: new FormControl(0),
+      tipoIVA: new FormControl(0.21),
+      importeIVA: new FormControl(0),
+      totalFactura: new FormControl(0)
     })
+    this.actualizarFactura();
+  }
+
+  actualizarFactura():void {
+    this.formFactura.valueChanges
+                    .subscribe(data => {
+                      const importeIVA = new Big(data.baseImponible).times(data.tipoIVA);
+                      const totalFactura = new Big(data.baseImponible).add(importeIVA);
+                      this.formFactura.get('importeIVA')?.patchValue(importeIVA, {emitEvent: false});
+                      this.formFactura.get('totalFactura')?.patchValue(totalFactura, {emitEvent: false});
+                    })
+  }
+
+  addFactura(): void {
+    const {['importeIVA']: removeImporte, ['totalFactura']: removeTotalFactura, ...rest} = this.formFactura.value;
+    const valorFactura = {...rest, user: 'Juan Pepinillos'};
+    console.log(valorFactura);
   }
 
 }
