81330c3b07c81ea22a64bdfe82e8b5499f6cd1a3
Samuel Sanz <samuel.sanz@gruposantander.com>
2022-03-10 12:44:30 +0100

    Version final del p09

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TablaClientesComponent } from './clientes/tabla-clientes/tabla-clientes.component';
+import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
+import { StatsComponent } from './stats/stats/stats.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TablaClientesComponent,
+    CrearClienteComponent,
+    StatsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
